6ac8155e82793c13469fb5cc4cafcde7a1df4fc2
tie052312 <tie052312@S1611C11952-T1.tisnt.tis.co.jp>
2018-06-22 21:45:38 +0900

    commit

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -6,27 +6,11 @@ import java.util.List;
 public class App {
 	public static void main(String[] args) {
 
-		String subCommand = null;
-		String param = null;
-
-		subCommand = args[0];
-		param = args[1];
+		String subCommand = args[0];
+		String param = args[1];
 
 		if (subCommand.equals("encode")) {
 
-//		    int parameter = Integer.parseInt(param);
-//			int sum = 0;
-//
-//			int count = 0;
-//
-//			for (int i = parameter; i > 9;) {
-//
-//				i = i / 9;
-//
-//				count += 1;
-//
-//
-//			}
 
 		} else if(subCommand.equals("decode")) {
 
@@ -42,7 +26,35 @@ public class App {
 
 			for (int i = 0, l = list.size(); i < l; i++) {
 
-				cal = convertAlphabettoNumber(list.get(i));
+				if (list.get(i).equals("A")) {
+
+					cal= 0;
+
+				} else if (list.get(i).equals("B")) {
+
+					cal= 1;
+				} else if (list.get(i).equals("C")) {
+
+					cal= 2;
+				} else if (list.get(i).equals("D")) {
+
+					cal= 3;
+				} else if (list.get(i).equals("E")) {
+
+					cal= 4;
+				} else if (list.get(i).equals("F")) {
+
+					cal= 5;
+				} else if (list.get(i).equals("G")) {
+
+					cal= 6;
+				} else if (list.get(i).equals("H")) {
+
+					cal= 7;
+				} else if (list.get(i).equals("I")) {
+
+					cal= 8;
+				}
 
 				for (int t = len ; t > 1; --t ) {
 
@@ -61,93 +73,6 @@ public class App {
 		} else if(subCommand.equals("align")) {
 
 
-
 		}
-
 	}
-
-	public static int convertAlphabettoNumber(String alphabet) {
-		int result = 0;
-
-		if (alphabet.equals("A")) {
-
-			result= 0;
-
-		} else if (alphabet.equals("B")) {
-
-			result= 1;
-		} else if (alphabet.equals("C")) {
-
-			result= 2;
-		} else if (alphabet.equals("D")) {
-
-			result= 3;
-		} else if (alphabet.equals("E")) {
-
-			result= 4;
-		} else if (alphabet.equals("F")) {
-
-			result= 5;
-		} else if (alphabet.equals("G")) {
-
-			result= 6;
-		} else if (alphabet.equals("H")) {
-
-			result= 7;
-		} else if (alphabet.equals("I")) {
-
-			result= 8;
-		}
-		return result;
-
-
-	}
-
-	public static String convertNumbertoAlphabet(int num) {
-		String result = null;
-
-		if (num == 0) {
-
-			result= "A";
-
-		} else if  (num == 1)  {
-
-			result= "B";
-
-		} else if  (num == 2)  {
-
-			result= "C";
-
-		} else if  (num == 3)  {
-
-			result= "D";
-
-		} else if  (num == 4)  {
-
-			result= "E";
-
-		} else if  (num == 5)  {
-
-			result= "F";
-
-		} else if  (num == 6)  {
-
-			result= "G";
-
-		} else if  (num == 7)  {
-
-			result= "H";
-
-		} else if  (num == 8)  {
-
-			result= "I";
-
-		}
-
-		return result;
-
-
-	}
-
-
 }
