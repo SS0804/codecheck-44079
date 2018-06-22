f98e03c07ecf36cab51bb80489f0eecc8d904a0f
tie052312 <tie052312@S1611C11952-T1.tisnt.tis.co.jp>
2018-06-22 19:26:17 +0900

    commit

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -16,23 +16,45 @@ public class App {
 		System.out.println("param:" + param);
 
 
-		List<String> list = new ArrayList<String>();
-
-
-
-		for (int i = 0, l = param.length(); i < l; i++) {
-
-			System.out.println(String.valueOf(param.charAt(i)));
-
-			list.add(String.valueOf(param.charAt(i)));
-		}
 
 		if (subCommand.equals("encode")) {
+
+//		    int parameter = Integer.parseInt(param);
+//			int sum = 0;
+//
+//			int count = 0;
+//
+//			for (int i = parameter; i > 9;) {
+//
+//				i = i / 9;
+//
+//				count += 1;
+//
+//
+//			}
+
+
+
+
+
+
+
+
+
 
 
 
 
 		} else if(subCommand.equals("decode")) {
+
+			List<String> list = new ArrayList<String>();
+
+			for (int i = 0, l = param.length(); i < l; i++) {
+
+				System.out.println(String.valueOf(param.charAt(i)));
+
+				list.add(String.valueOf(param.charAt(i)));
+			}
 
 			int result = 0;
 			int cal = 0;
@@ -48,8 +70,6 @@ public class App {
 
 				}
 
-				System.out.println("計算結果:" + cal);
-
 				result += cal;
 
 				--len;
@@ -58,19 +78,11 @@ public class App {
 
 			System.out.println(result);
 
-
 		} else if(subCommand.equals("align")) {
 
 
 
 		}
-
-
-
-
-
-
-
 
 	}
 
@@ -106,6 +118,52 @@ public class App {
 
 			result= 8;
 		}
+		return result;
+
+
+	}
+
+	public static String convertNumbertoAlphabet(int num) {
+		String result = null;
+
+		if (num == 0) {
+
+			result= "A";
+
+		} else if  (num == 1)  {
+
+			result= "B";
+
+		} else if  (num == 2)  {
+
+			result= "C";
+
+		} else if  (num == 3)  {
+
+			result= "D";
+
+		} else if  (num == 4)  {
+
+			result= "E";
+
+		} else if  (num == 5)  {
+
+			result= "F";
+
+		} else if  (num == 6)  {
+
+			result= "G";
+
+		} else if  (num == 7)  {
+
+			result= "H";
+
+		} else if  (num == 8)  {
+
+			result= "I";
+
+		}
+
 		return result;
 
 
