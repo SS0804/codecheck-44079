4c2c08b073d9322c8ccadd9dbefd0b603706bf8f
tie052312 <tie052312@S1611C11952-T1.tisnt.tis.co.jp>
2018-06-22 18:47:29 +0900

    commit

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -1,10 +1,115 @@
 package codecheck;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
 	public static void main(String[] args) {
-		for (int i = 0, l = args.length; i < l; i++) {
-			String output = String.format("argv[%s]: %s", i, args[i]);
-			System.out.println(output);
+
+		String subCommand = null;
+		String param = null;
+
+		subCommand = args[0];
+		param = args[1];
+
+		System.out.println("subCommand:" + subCommand);
+		System.out.println("param:" + param);
+
+
+		List<String> list = new ArrayList<String>();
+
+
+
+		for (int i = 0, l = param.length(); i < l; i++) {
+
+			System.out.println(String.valueOf(param.charAt(i)));
+
+			list.add(String.valueOf(param.charAt(i)));
 		}
+
+		if (subCommand.equals("encode")) {
+
+
+
+
+		} else if(subCommand.equals("decode")) {
+
+			int result = 0;
+			int cal = 0;
+
+			for (int i = 0, l = list.size(); i < l; i++) {
+
+				cal = convertAlphabettoNumber(list.get(i));
+
+				for (int len = list.size() ; len > 1; --len ) {
+
+					cal = cal * 9;
+
+				}
+
+				System.out.println("計算結果:" + cal);
+
+				result =+ cal;
+
+			}
+
+			System.out.println(result);
+
+
+
+
+
+		} else if(subCommand.equals("align")) {
+
+
+
+		}
+
+
+
+
+
+
+
+
 	}
+
+	public static int convertAlphabettoNumber(String alphabet) {
+		int result = 0;
+
+		if (alphabet.equals("A")) {
+
+			result= 0;
+
+		} else if (alphabet.equals("B")) {
+
+			result= 1;
+		} else if (alphabet.equals("C")) {
+
+			result= 2;
+		} else if (alphabet.equals("D")) {
+
+			result= 3;
+		} else if (alphabet.equals("E")) {
+
+			result= 4;
+		} else if (alphabet.equals("F")) {
+
+			result= 5;
+		} else if (alphabet.equals("G")) {
+
+			result= 6;
+		} else if (alphabet.equals("H")) {
+
+			result= 7;
+		} else if (alphabet.equals("I")) {
+
+			result= 8;
+		}
+		return result;
+
+
+	}
+
+
 }
